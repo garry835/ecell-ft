bc9505dce6ae06f92c745cba70dea84422e8b9d1
garry835 <tatiagarvv@gmail.com>
2024-09-07 16:47:19 +0530

    hosting

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id='h'>MY DAILY TASKS</h1>
+      <TodoWrapper  /> 
+      
+    </div>
+    
+  
+  );
+}
+
+
+
+
+
+export default App;
